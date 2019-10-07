86e400ee5f2d01e4f527553650dcd27db06a1800
Steinalicious <aso40@hi.is>
2019-10-07 12:51:14 +0000

    bætti við entities klösum

--- src/main/java/project/persistence/entities/Course.java
+++ src/main/java/project/persistence/entities/Course.java
@@ -1,4 +1,26 @@
 package project.persistence.entities;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "courses")
 public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
